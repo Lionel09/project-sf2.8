32789a3ce870d6c15fd05a9e47ae0cdb82dc3561
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2016-02-19 15:17:23 +0100

    comment getOptionsCmsTypes to throws edit error

--- src/AdminBundle/Form/Type/CmsContent/EditType.php
+++ src/AdminBundle/Form/Type/CmsContent/EditType.php
@@ -15,12 +15,13 @@ class EditType extends BaseEditType
      * @param  array $builderOptions The builder options.
      * @return array Field options.
      */
-//     protected function getOptionsCmsTypes(array $builderOptions = array())
-//     {
-//         $optionsClass = '\AdminBundle\Form\Type\CmsContent\Options';
-//         $options = class_exists($optionsClass) ? new $optionsClass() : null;
-// 
-//         return $this->resolveOptions('cms_types', array(  'label' => 'Cms types',  'translation_domain' => 'Admin',  '// required' => false,  'multiple' => true,  'class' => 'Model\\CmsType',  'query' => \Model\CmsTypeQuery::create()// ->orderById(),), $builderOptions, $options);
-//     }
+    /*
+    protected function getOptionsCmsTypes(array $builderOptions = array())
+    {
+        $optionsClass = '\AdminBundle\Form\Type\CmsContent\Options';
+        $options = class_exists($optionsClass) ? new $optionsClass() : null;
 
+        return $this->resolveOptions('cms_types', array(  'label' => 'Cms types',  'translation_domain' => 'Admin',  'required' => false,  'multiple' => true,  'class' => 'Model\\CmsType',  'query' => \Model\CmsTypeQuery::create()->orderById(),), $builderOptions, $options);
+    }
+    */
 }
